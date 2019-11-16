feb18a05cd50d40e71f8e53bd3998cb337f017b1
daniel.crawford <daniel.crawford@audionow.com>
2019-11-16 17:28:34 -0500

    Fix more indentation

--- src/interealmGames/color/rgba/composer/RgbaComposer.hx
+++ src/interealmGames/color/rgba/composer/RgbaComposer.hx
@@ -11,23 +11,23 @@ import StringTools;
  * @TODO handle alpha
  */
 class RgbaComposer implements Composer<Color> {
-    var precision:Int;
-    var rounding:Null<Rounding>;
+	var precision:Int;
+	var rounding:Null<Rounding>;
 
 	public function new (precision:Int = 0, rounding:Null<Rounding> = null) {
-        this.precision = precision;
-        this.rounding = rounding;
-    }
+		this.precision = precision;
+		this.rounding = rounding;
+	}
 
-    /**
+	/**
 	 * Converts the color to a string
 	 * @param color The color to be composed
 	 * @return The formatted color
 	 */
 	public function compose(color:Color):String {
-        var alpha = this.rounding == null 
-            ? color.alpha 
-            : Math.roundTo(color.alpha, this.precision, this.rounding);
+		var alpha = this.rounding == null 
+			? color.alpha 
+			: Math.roundTo(color.alpha, this.precision, this.rounding);
 		return "rgb(" + color.red + ", "
 			+ color.green + ", "
 			+ color.blue + ", "
